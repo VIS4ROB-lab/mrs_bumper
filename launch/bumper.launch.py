c95243b204ff49e624d20cee2c69ee8c84b950aa
Rowan Border <rowanjborder@gmail.com>
2025-12-09 11:17:12 +0200

    update bumper launch

--- launch/bumper.launch.py
+++ launch/bumper.launch.py
@@ -36,20 +36,11 @@ def generate_launch_description():
             os.path.join(mrs_bumper_dir, "config/realworld.yaml"),
             {
                 "uav_name": LaunchConfiguration("uav_name"),
-                "path_to_mask": LaunchConfiguration("mask_filename"),
                 "frame_id": LaunchConfiguration("fcu_horizontal_frame"),
             }
         ],
         remappings=[
-            # Laser rangefinder topics
-            ("lidar1d_down_in", "hw_api/distance_sensor"),
-            ("lidar1d_up_in", "garmin_up/range"),
-            # Other input topics
-            ("depthmap_in", "front_rgbd/aligned_depth_to_color/image_raw"),
-            ("depth_cinfo_in", "front_rgbd/aligned_depth_to_color/camera_info"),
             ("lidar3d_in", "/livox/lidar"),
-            ("lidar2d_in", "rplidar/scan"),
-            # Output topics
             ("obstacle_sectors_out", "~obstacle_sectors"),
         ]
     )
