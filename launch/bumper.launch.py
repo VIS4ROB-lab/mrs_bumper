2b7c6a30516dfa25895cdc8357c602ba8cc12a49
Rowan Border <rowanjborder@gmail.com>
2026-03-03 17:23:44 +0200

    update launch for obstacle bumper

--- launch/bumper.launch.py
+++ launch/bumper.launch.py
@@ -40,7 +40,7 @@ def generate_launch_description():
             }
         ],
         remappings=[
-            ("lidar3d_in", "/livox/lidar"),
+            ("lidar3d_in", "/" + LaunchConfiguration("uav_name") + "/livox/lidar"),
             ("obstacle_sectors_out", "~obstacle_sectors"),
         ]
     )
